79855b146a81058a6a035f0abf249bbfe9577f26
Misha Martin <90650953+MishaMartin@users.noreply.github.com>
2022-02-18 19:49:51 -0700

    Added typed error 'raise' instead of 'return' for add_reminder in database.py

--- src/database.py
+++ src/database.py
@@ -16,7 +16,7 @@ def list_reminders():
 def add_reminder(text, date, ReminderClass):
     reminder = ReminderClass(text, date)
     if not isinstance(reminder, DeadlinedReminder):
-        return TypeError('Invalid Reminder Class')
+        raise TypeError('Invalid Reminder Class')
     with open('reminders.csv', 'a+', newline='\n') as file:
         writer = csv.writer(file)
         writer.writerow(reminder)
